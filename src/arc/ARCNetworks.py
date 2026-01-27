ae01e6ea65660a7f58fdc5592657640e382b7abb
Reid Taylor <rebridgers@gmail.com>
2026-01-27 16:11:59 -0600

    Solution

--- src/arc/ARCNetworks.py
+++ src/arc/ARCNetworks.py
@@ -144,8 +144,7 @@ class Encoder(torch.nn.Module):
 
         final_layer = self.fc_out(attended_layers)
 
-        masks_tensor = torch.stack(masks)
-        print(final_layer.shape, masks_tensor.shape)
+        masks_tensor = torch.stack(masks,dim=1)
         return torch.cat([final_layer, masks_tensor], dim=-1)
 
 @beartype
