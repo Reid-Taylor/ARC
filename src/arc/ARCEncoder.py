215717ce43efb14d335442501d56790d65455684
Reid Taylor <rebridgers@gmail.com>
2026-01-27 18:10:32 -0600

    Removed mirrored loss estimations due to interference caused by augmentations

--- src/arc/ARCEncoder.py
+++ src/arc/ARCEncoder.py
@@ -245,14 +245,10 @@ class MultiTaskEncoder(L.LightningModule):
         for key in self.downstream_attributes:
             key_to_idx[f'downstream_{key}'] = len(key_to_idx)
             downstream_attribute_loss.append(
-                (F.mse_loss(
+                F.mse_loss(
                     results["standard"]["predicted_downstream_attributes"][key],
                     batch[key]
-                ) + 
-                F.mse_loss(
-                    results["mirrored"]["predicted_downstream_attributes"][key],
-                    batch[key]
-                )) * 0.5
+                )
             ) 
 
         # Task Sensitive Attribute Detection Loss
@@ -260,14 +256,10 @@ class MultiTaskEncoder(L.LightningModule):
         for key in self.task_sensitives:
             key_to_idx[f'sensitive_{key}'] = len(key_to_idx)
             task_sensitive_loss.append(
-                (F.binary_cross_entropy(
+                F.binary_cross_entropy(
                     results['standard']["predicted_task_sensitive_attributes"][key], 
                     batch[f"presence_{key}"]
-                ) + 
-                F.binary_cross_entropy(
-                    results['mirrored']["predicted_task_sensitive_attributes"][key], 
-                    batch[f"presence_{key}"]
-                )) * 0.5
+                )
             )
         
         # Task Invariant Loss
@@ -275,14 +267,10 @@ class MultiTaskEncoder(L.LightningModule):
         for key in self.task_invariants:
             key_to_idx[f'invariant_{key}'] = len(key_to_idx)
             task_invariant_loss.append(
-                (F.mse_loss(
+                F.mse_loss(
                     results['standard']["online_representation"], 
                     results['standard']["target_representation"].detach()
-                ) + 
-                F.mse_loss(
-                    results['mirrored']["online_representation"], 
-                    results['mirrored']["target_representation"].detach()
-                ))*0.5
+                )
             )
 
         loss = torch.stack([reconstruction_loss] + downstream_attribute_loss + task_sensitive_loss + task_invariant_loss)
