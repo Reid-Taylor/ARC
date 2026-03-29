6a9a1925ab590bb077d6f7030aaa8197753cc91b
Reid Taylor <rebridgers@gmail.com>
2026-03-29 12:08:09 -0500

    Config changes for experimentation

--- scripts/train_contrastive_encoder.py
+++ scripts/train_contrastive_encoder.py
@@ -181,7 +181,7 @@ def setup_trainer(
     
     early_stopping = EarlyStopping(
         monitor="Validation/Validation Loss",
-        patience=5,
+        patience=7,
         mode="min"
     )
     
@@ -201,7 +201,7 @@ def setup_trainer(
         accelerator=accelerator,
         devices=devices,
         log_every_n_steps=1,
-        check_val_every_n_epoch=3,
+        check_val_every_n_epoch=5,
         enable_progress_bar=True,
         enable_model_summary=True
     )
